ccd34c8e1aeea21629724aadf7920dad7883fdf3
AYAN754 <ayanalizai960@gmail.com>
2024-11-03 13:54:59 +0500

    lesson6

---
+++ activity_1.py
@@ -0,0 +1,5 @@
+a = int(input("enter the number of which youwant the sum: "))
+sum = 0
+for i in range(1,a+1):
+    sum = sum+i
+print("the sum is: ", sum)
---
+++ activity_2.py
@@ -0,0 +1,6 @@
+word = str(input("enter the word that you want to reveres: "))
+word1 = ""
+for i in word:
+    word1 = i+word1
+print("the orignal string was: ", word)
+print("the reversed string is: ", word1)
